f6896c820ba5b6cf7fc97a86a8b277ab834ae68a
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-03 00:39:17 +0900

    Create beautifyJson.test.ts

---
+++ src/json/beautifyJson.test.ts
@@ -0,0 +1,17 @@
+import {
+  describe,
+  expect,
+  it
+} from '@jest/globals'
+import { beautifyJson } from './beautifyJson'
+
+describe('beautifyJson', () => {
+  it('returns beautified JSON', async () => {
+    const actual = beautifyJson('{"string":"string","number":1}')
+
+    expect(actual).toBe(`{
+  "string": "string",
+  "number": 1
+}`)
+  })
+})
