6e2a39b4990a02063b76ae6ffb4b8bd984acf6e0
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 14:07:22 +0900

    Create generate.ts

---
+++ src/generate.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { generateWordClouds } from './generateWordClouds'
+
+console.log('Start generate')
+generateWordClouds()
+  .then(() => {
+    console.log('End generate')
+  })
+  .catch((error: unknown) => {
+    console.error(error)
+    throw error
+  })
