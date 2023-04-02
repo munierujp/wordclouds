cce6f2a94b99e404c601ff7463d10e46cacf7a9c
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 20:11:09 +0900

    Add resize option (#13)
    * Add sharp package
    
    * Add maxWidth
    
    * Create resizeImage.ts
    
    * Fix resizeImage
    
    * Update generateWordCloud
    
    * Update data
    
    * Rename resizeImage to resizeImageFile

---
+++ src/image/resizeImageFile.ts
@@ -0,0 +1,17 @@
+import sharp from 'sharp'
+
+export const resizeImageFile = async (path: string, maxWidth: number): Promise<void> => {
+  const input = sharp(path)
+  const { width } = await input.metadata()
+
+  if (width === undefined) {
+    return
+  }
+
+  if (width <= maxWidth) {
+    return
+  }
+
+  const buffer = await input.resize({ width: maxWidth }).toBuffer()
+  await sharp(buffer).toFile(path)
+}
--- src/wordclouds/generateWordCloud.ts
+++ src/wordclouds/generateWordCloud.ts
@@ -1,4 +1,5 @@
 import type { Page } from 'playwright'
+import { resizeImageFile } from '../image'
 import { logger } from '../logger'
 import { downloadImageFile } from './downloadImageFile'
 import { downloadWordCloudFile } from './downloadWordCloudFile'
@@ -9,10 +10,18 @@ import { uploadWordListFile } from './uploadWordListFile'
 export const generateWordCloud = async (page: Page, profile: Profile): Promise<void> => {
   logger.info(`Upload ${profile.name}.wcld2 file`)
   await uploadWordCloudFile(page, `data/${profile.name}.wcld2`)
+
   logger.info('Upload words.csv file')
   await uploadWordListFile(page, 'data/words.csv')
+
   logger.info(`Download ${profile.name}.${profile.type} file`)
   await downloadImageFile(page, profile)
+
+  if (profile.maxWidth !== undefined) {
+    logger.info(`Resize ${profile.name}.${profile.type} file`)
+    await resizeImageFile(`data/${profile.name}.${profile.type}`, profile.maxWidth)
+  }
+
   logger.info(`Download ${profile.name}.wcld2 file`)
   await downloadWordCloudFile(page, `data/${profile.name}.wcld2`)
 }
