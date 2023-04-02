7f722cb27b2285ed9fbe15d2a0355abab9269886
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 15:47:23 +0900

    Update Selector

--- src/Selector.ts
+++ src/Selector.ts
@@ -1,15 +1,17 @@
 export const Selector = {
+  ApplyWordListButton: '#offcanvasWordList .applyBtn',
   DownloadButton: '#toolControls .download .btn',
   FileMenuLink: '#wizFileMenu .nav-link',
   ImportCsvLink: '#wordlistMenu .dropdown-item[data-task="importCSV"]',
   JpgButton: '#toolControls .btn[data-filetype="jpg"]',
   Modal: '#myModal',
   OpenWordCloudLink: '#fileMenu .dropdown-item[data-task="openWordcloud"]',
+  OpenWordListLink: '#toolControls .browseBtn',
   PngButton: '#toolControls .btn[data-filetype="png"]',
   RenderToast: '#renderToast',
   SaveButton: '#applyBtn',
   SaveImageLink: '#fileMenu .dropdown-item[data-task="saveImageAs,hd"]',
-  ToolControls: '#toolControls',
+  SaveWordCloudLink: '#fileMenu .dropdown-item[data-task="saveWordcloud"]',
   WordListLink: '#wizWordList .nav-link'
 } as const
 // eslint-disable-next-line @typescript-eslint/no-redeclare
