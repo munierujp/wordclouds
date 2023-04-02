3efc7e6f180f8568bd9c32c4c5549e82bb686403
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 15:50:52 +0900

    Update downloadWordCloudFile

--- src/downloadWordCloudFile.ts
+++ src/downloadWordCloudFile.ts
@@ -3,8 +3,9 @@ import { Selector } from './Selector'
 
 export const downloadWordCloudFile = async (page: Page, filePath: string): Promise<void> => {
   await page.click(Selector.FileMenuLink)
-  const downloadPromise = page.waitForEvent('download')
   await page.click(Selector.SaveWordCloudLink)
+  const downloadPromise = page.waitForEvent('download')
+  await page.click(Selector.SaveButton)
   const download = await downloadPromise
   await download.saveAs(filePath)
 }
