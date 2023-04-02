4acd6d56ebab79546db49f8ff019195665691383
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 15:17:01 +0900

    Create downloadImageFile.ts

---
+++ src/downloadImageFile.ts
@@ -0,0 +1,15 @@
+import type { Page } from 'playwright'
+import { FileType } from './FileType'
+import type { Profile } from './Profile'
+import { Selector } from './Selector'
+
+export const downloadImageFile = async (page: Page, profile: Profile): Promise<void> => {
+  await page.click(Selector.FileMenuLink)
+  await page.click(Selector.SaveImageLink)
+  await page.click(profile.type === FileType.Jpg ? Selector.JpgButton : Selector.PngButton)
+  await page.click(Selector.SaveButton)
+  const downloadPromise = page.waitForEvent('download')
+  await page.click(Selector.DownloadButton)
+  const download = await downloadPromise
+  await download.saveAs(profile.output)
+}
