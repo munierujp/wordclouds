395ddebc06d806de7ff2878b86a50132b6da87f1
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 13:19:36 +0900

    Remove main.ts

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Update `wordList` of `.wcld2` files using `words.csv`
-
-// TODO: Run headless browser
-
-// TODO: Open https://www.wordclouds.com/
-
-// TODO: Upload `.wcld2` file
-
-// TODO: Download image to `data` dir
