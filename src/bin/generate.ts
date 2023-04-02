544fcb60872db4a862fd93d5cbe48d03bee9ec55
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-02 14:39:57 +0900

    Create bin dir

--- src/bin/generate.ts
+++ src/bin/generate.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import { generateWordClouds } from './generateWordClouds'
+import { generateWordClouds } from '../generateWordClouds'
 
-console.log('Start generate')
+console.log('Start generate script')
 generateWordClouds()
   .then(() => {
-    console.log('End generate')
+    console.log('End generate script')
   })
   .catch((error: unknown) => {
     console.error(error)
