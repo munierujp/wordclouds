f4d3ee106bedd511e605587f87d70b02092b98ab
Munieru <20086673+munierujp@users.noreply.github.com>
2023-06-14 00:05:02 +0900

    Add `--profile` option

--- src/core/generateWordCloudFiles.ts
+++ src/core/generateWordCloudFiles.ts
@@ -5,13 +5,19 @@ import { profiles } from '../profile'
 import { generateWordCloudFile } from './generateWordCloudFile'
 
 export const generateWordCloudFiles = async (options: Options): Promise<void> => {
+  const targetProfiles = options.profile === undefined ? profiles : profiles.filter((profile) => profile.name === options.profile)
+
+  if (targetProfiles.length === 0) {
+    throw new Error('There are no profiles.')
+  }
+
   logger.info('Launch browser')
   const browser = await chromium.launch({ headless: options.headless })
   const page = await browser.newPage()
   logger.info('Open https://www.wordclouds.com/')
   await page.goto('https://www.wordclouds.com/')
 
-  for (const profile of profiles) {
+  for (const profile of targetProfiles) {
     await generateWordCloudFile(page, profile)
   }
 
