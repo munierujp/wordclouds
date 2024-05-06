8e3fddb0869e9c7169fc2360de6dbc54c2f064f2
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-06 20:33:37 +0900

    Fix errors

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import { generateWordCloudFiles } from './core/index.js'
 import { logger } from './logger/index.js'
 import { initOptions } from './options/index.js'
 
-const options = initOptions()
-logger.level = options.logLevel
-logger.info('Start')
-logger.debug(`options=${JSON.stringify(options)}`)
-
-generateWordCloudFiles(options)
-  .then(() => {
-    logger.info('End')
-  })
-  .catch((error: unknown) => {
-    logger.fatal(error)
-    throw error
-  })
+try {
+  const options = initOptions()
+  logger.level = options.logLevel
+  logger.info('Start')
+  logger.debug(`options=${JSON.stringify(options)}`)
+  await generateWordCloudFiles(options)
+  logger.info('End')
+} catch (error) {
+  logger.fatal(error)
+  throw error
+}
