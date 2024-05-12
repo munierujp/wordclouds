d978c3d0020e7fd0f3a179a6f19c55f0a57eaa41
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-12 23:55:56 +0900

    Replace Jest with Node.js's test runner (#247)
    * Add glob
    
    * Replace Jest with Node.js's test runner
    
    * Remove jest

--- src/core/beautifyJsonFile.ts
+++ src/core/beautifyJsonFile.ts
@@ -1,9 +1,11 @@
-import { readFile } from '../file/readFile.js'
-import { writeFile } from '../file/writeFile.js'
+import {
+  readFile,
+  writeFile
+} from 'node:fs/promises'
 import { beautifyJson } from '../json/beautifyJson.js'
 
 export const beautifyJsonFile = async (path: string): Promise<void> => {
-  const json = await readFile(path)
+  const json = await readFile(path, 'utf8')
   const beautifiedJson = beautifyJson(json)
   await writeFile(path, beautifiedJson)
 }
--- src/file/writeFile.ts
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs'
-
-export const writeFile = async (path: string, data: string): Promise<void> => {
-  await fs.promises.writeFile(path, data)
-}
