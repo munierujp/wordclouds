3997bbec779b11c0b19454d7b9382875d05a2dbd
Munieru <20086673+munierujp@users.noreply.github.com>
2025-05-19 21:05:48 +0900

    `結束バンド`を追加 (#353)
    * `結束バンド`を追加
    
    * update selectors
    
    * add logs
    
    * fix selector
    
    * update data
    
    * refactoring

--- src/core/uploadWordListFile.ts
+++ src/core/uploadWordListFile.ts
@@ -1,12 +1,19 @@
 import type { Page } from 'playwright'
+import { logger } from '../logger/logger.js'
 import { Selector } from './Selector.js'
 
 export const uploadWordListFile = async (page: Page, path: string): Promise<void> => {
+  logger.debug(`[uploadWordListFile] click ${Selector.WordListLink}`)
   await page.click(Selector.WordListLink)
+  logger.debug(`[uploadWordListFile] click ${Selector.ImportCsvLink}`)
   await page.click(Selector.ImportCsvLink)
   const fileChooserPromise = page.waitForEvent('filechooser')
+  logger.debug(`[uploadWordListFile] click ${Selector.OpenWordListLink}`)
   await page.click(Selector.OpenWordListLink)
   const fileChooser = await fileChooserPromise
   await fileChooser.setFiles(path)
-  await page.click(Selector.ApplyWordListButton)
+  logger.debug(`[uploadWordListFile] click ${Selector.WordListApplyButton}`)
+  await page.click(Selector.WordListApplyButton)
+  logger.debug(`[uploadWordListFile] click ${Selector.WordListCloseButton}`)
+  await page.click(Selector.WordListCloseButton)
 }
