afa705aefc831300c8b4ad14557471a991f7fcea
Munieru <20086673+munierujp@users.noreply.github.com>
2025-05-19 20:14:54 +0900

    add logs

--- src/core/downloadImageFile.ts
+++ src/core/downloadImageFile.ts
@@ -1,15 +1,22 @@
 import type { Page } from 'playwright'
+import { logger } from '../logger/logger.js'
 import { FileType } from '../profile/FileType.js'
 import { Selector } from './Selector.js'
 
 export const downloadImageFile = async (page: Page, path: string, type: FileType): Promise<void> => {
+  logger.debug(`[downloadImageFile] click ${Selector.FileMenuLink}`)
   await page.click(Selector.FileMenuLink)
+  logger.debug(`[downloadImageFile] click ${Selector.SaveImageLink}`)
   await page.click(Selector.SaveImageLink)
+  logger.debug(`[downloadImageFile] click ${type === FileType.Jpg ? Selector.FileTypeJpgButton : Selector.FileTypePngButton}`)
   await page.click(type === FileType.Jpg ? Selector.FileTypeJpgButton : Selector.FileTypePngButton)
+  logger.debug(`[downloadImageFile] click ${Selector.SaveButton}`)
   await page.click(Selector.SaveButton)
   const downloadPromise = page.waitForEvent('download')
+  logger.debug(`[downloadImageFile] click ${Selector.DownloadButton}`)
   await page.click(Selector.DownloadButton)
   const download = await downloadPromise
   await download.saveAs(path)
+  logger.debug(`[downloadImageFile] click ${Selector.CloseButton}`)
   await page.click(Selector.CloseButton)
 }
