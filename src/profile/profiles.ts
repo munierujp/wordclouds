b4b3a25d1c479d3c345e90690fa32d0e7f61d1c6
Munieru <20086673+munierujp@users.noreply.github.com>
2026-07-07 01:32:40 +0900

    プロファイルのコメントアウトされたエントリを整理

--- src/profile/profiles.ts
+++ src/profile/profiles.ts
@@ -2,14 +2,14 @@ import { FileType } from './FileType.ts'
 import type { Profile } from './Profile.ts'
 
 export const profiles: Profile[] = [
-  {
-    name: 'google-slides-dark',
-    type: FileType.Png
-  },
-  {
-    name: 'google-slides-light',
-    type: FileType.Png
-  },
+  // {
+  //   name: 'google-slides-dark',
+  //   type: FileType.Png
+  // },
+  // {
+  //   name: 'google-slides-light',
+  //   type: FileType.Png
+  // },
   {
     name: 'square',
     type: FileType.Png
@@ -23,8 +23,8 @@ export const profiles: Profile[] = [
     name: 'x',
     type: FileType.Png
   },
-  {
-    name: 'zoom',
-    type: FileType.Png
-  }
+  // {
+  //   name: 'zoom',
+  //   type: FileType.Png
+  // }
 ]
